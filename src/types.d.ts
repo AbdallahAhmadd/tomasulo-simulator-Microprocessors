f28715ca4b7b0775c6207ce644ae3ba7b8790a33
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-12-11 02:31:24 +0200

    feat: add Instructions enum and update instruction interface for clarity

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,9 @@
 export interface instruction {
     opcode: string;
+    rd: string;
     rs: string;
     rt: string;
-    rd: string;
-    isLoop?:Boolean;
+    labelAddress?:string;
 }
 
 export interface ReservationStation {
@@ -47,28 +47,7 @@ export interface instructionEntry{
     writeResult: number;
 }
 
-export enum Instructions {
-    DADDI = "DADDI",
-    DSUBI = "DSUBI",
-    ADD_D = "ADD.D",
-    ADD_S = "ADD.S",
-    SUB_D = "SUB.D",
-    SUB_S = "SUB.S",
-    MUL_D = "MUL.D",
-    MUL_S = "MUL.S",
-    DIV_D = "DIV.D",
-    DIV_S = "DIV.S",
-    LW = "LW",
-    LD = "LD",
-    L_S = "L.S",
-    L_D = "L.D",
-    SW = "SW",
-    SD = "SD",
-    S_S = "S.S",
-    S_D = "S.D",
-    BNE = "BNE",
-    BEQ = "BEQ",
-}
+
 
 export interface latencies {
     DADDI : number,
