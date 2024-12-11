89c19308d76001a775303af725b858e4ed920345
Farahh23 <125885412+Farahh23@users.noreply.github.com>
2024-12-11 20:52:11 +0200

    refactor: change opcoce type to instruction

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,8 @@
+import { Cache } from "./cache";
+import { Memory } from "./hardwareComponents/Memory";
+import {Instructions} from "./enums.ts";
 export interface instruction {
-    opcode: string;
+    opcode: Instructions;
     rd: string;
     rs: string;
     rt: string;
@@ -7,63 +10,63 @@ export interface instruction {
 }
 
 export interface ReservationStation {
-    tag: string;
-    busy: boolean;
-    op: string;
-    vj: number;
-    vk: number;
-    qj: string;
-    qk: string;
-    A: number;
-    timeRemaining: number;
+  tag: string;
+  busy: boolean;
+  op: string;
+  vj: number;
+  vk: number;
+  qj: string;
+  qk: string;
+  A: number;
+  timeRemaining: number;
 }
 
 export interface LoadBuffer {
-    tag: string;
-    busy: boolean;
-    address: number;
-    timeRemaining: number;
+  tag: string;
+  busy: boolean;
+  address: number;
+  timeRemaining: number;
 }
 
 export interface StoreBuffer {
-    tag: string;
-    busy: boolean;
-    address: number;
-    v: number;
-    q: string;
-    timeRemaining: number;
+  tag: string;
+  busy: boolean;
+  address: number;
+  v: number;
+  q: string;
+  timeRemaining: number;
 }
 
 export interface registerFileEntry {
-    registerName: string;
-    Q: string;
-    value: number;
+  registerName: string;
+  Q: string;
+  value: number;
 }
 
-export interface instructionEntry{
-    instruction: instruction;
-    issue: number;
-    execution_complete: string;
-    writeResult: number;
+export interface instructionEntry {
+  instruction: instruction;
+  issue: number;
+  execution_complete: string;
+  writeResult: number;
 }
 
 
 
 export interface latencies {
-    DADDI : number,
-    DSUBI : number,
-    ADD_D : number,
-    ADD_S : number,
-    SUB_D : number,
-    SUB_S : number,
-    MUL_D : number,
-    MUL_S : number,
-    DIV_D : number,
-    DIV_S : number,
-    LW : number,
-    LD : number,
-    L_S : number,
-    L_D : number,
+  DADDI: number;
+  DSUBI: number;
+  ADD_D: number;
+  ADD_S: number;
+  SUB_D: number;
+  SUB_S: number;
+  MUL_D: number;
+  MUL_S: number;
+  DIV_D: number;
+  DIV_S: number;
+  LW: number;
+  LD: number;
+  L_S: number;
+  L_D: number;
     SW : number,
     SD : number,  
     S_S : number, 
@@ -73,35 +76,36 @@ export interface latencies {
 }
 
 export interface SystemState {
-    /* tomasulo architecture */
-    instructionQueue: instruction[];
-    fpAddReservationStations: ReservationStation[];
-    fpMulReservationStations: ReservationStation[];
-    intAddReservationStations: ReservationStation[];
-    intMulReservationStations: ReservationStation[];
-    loadBuffer: LoadBuffer[];
-    storeBuffer: StoreBuffer[];
-    fpRegisterFile: registerFileEntry[];
-    intRegisterFile: registerFileEntry[];
-    memory: number[];
-    cache: Cache;
-    /* simulation state */
-    instructionTable: instructionEntry[];
-    clockCycle: number;
-    pc: number;
-    latencies: latencies;
-
+  /* tomasulo architecture */
+  instructionQueue: instruction[];
+  fpAddReservationStations: ReservationStation[];
+  fpMulReservationStations: ReservationStation[];
+  intAddReservationStations: ReservationStation[];
+  intMulReservationStations: ReservationStation[];
+  loadBuffer: LoadBuffer[];
+  storeBuffer: StoreBuffer[];
+  fpRegisterFile: registerFileEntry[];
+  intRegisterFile: registerFileEntry[];
+  memory: Memory;
+  cache: Cache;
+  /* simulation state */
+  instructionTable: instructionEntry[];
+  clockCycle: number;
+  pc: number;
+  latencies: latencies;
+  notes: string;
 }
 
-
 export interface SystemConfig {
-    fpAddReservationStationsSize: number;
-    fpMulReservationStationsSize: number;
-    intAddReservationStationsSize: number;
-    intMulReservationStationsSize: number;
-    loadBufferSize: number;
-    storeBufferSize: number;
-    cacheSize: number;
-    blockSize: number;
-    latencies: latencies;
+  fpAddReservationStationsSize: number;
+  fpMulReservationStationsSize: number;
+  intAddReservationStationsSize: number;
+  intMulReservationStationsSize: number;
+  fpRegisterFileSize: number;
+  intRegisterFileSize: number;
+  loadBufferSize: number;
+  storeBufferSize: number;
+  cacheSize: number;
+  blockSize: number;
+  latencies: latencies;
 }
