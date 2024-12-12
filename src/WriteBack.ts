12d63bce0899bc89b749a4ad85393ef93463d417
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-12-12 05:39:52 +0200

    feat: enhance write back logic to handle multiple reservation stations and improve result broadcasting

--- src/WriteBack.ts
+++ src/WriteBack.ts
@@ -1,91 +1,119 @@
-import { SystemState } from "./types";
-import { broadcastResult , updateRegisterFiles } from "./helpers";
+import { ReservationStation, SystemState } from "./types";
 
 //=================================================================================================================//
 //												WRITE BACK                                                         //
 //=================================================================================================================//
-export function writeBack(systemState: SystemState): SystemState {
-    const {
-        fpAddReservationStations,
-        fpMulReservationStations,
-        loadBuffer,
-        fpRegisterFile,
-        intRegisterFile,
-        instructionTable,
-        clockCycle,
-    } = systemState;
+export function writeBack(newState: SystemState) {
 
-    let busValue: number | null = null; 
-    let busTag: string | null = null;  
+    //check if there is any instruction that is ready to be written and push it in an array
 
-//=================================================================================================================//
-//												ADD/SUB                                                            //
-//=================================================================================================================//
+    let toBeWrtten: ReservationStation[] = [];
+    newState.fpAddReservationStations.forEach((station, index) => {
+        if (station.busy && !station.qj && !station.qk && station.timeRemaining === 0 && station.result) {
+            toBeWrtten.push(station);
 
-    for (const station of fpAddReservationStations) {
-        if (station.busy && station.timeRemaining === 0) {
-            //MISSINNNNNGGGG
-           // busValue = //result mn el execute
-            busTag = station.tag; 
-            station.busy = false; 
-            break;
         }
+    })
+
+    newState.fpMulReservationStations.forEach((station, index) => {
+        if (station.busy && !station.qj && !station.qk && station.timeRemaining === 0 && station.result) {
+            toBeWrtten.push(station);
+        }
+    })
+
+    newState.intAddReservationStations.forEach((station, index) => {
+        if (station.busy && !station.qj && !station.qk && station.timeRemaining === 0 && station.result) {
+            toBeWrtten.push(station);
+        }
+    })
+
+    newState.intMulReservationStations.forEach((station, index) => {
+        if (station.busy && !station.qj && !station.qk && station.timeRemaining === 0 && station.result) {
+            toBeWrtten.push(station);
+        }
+    })
+
+    //if there is no instruction to be written return
+    if (toBeWrtten.length === 0)
+        return;
+
+    //array of counts equal in lenght of to be written filled with zeros
+    let counts = Array(toBeWrtten.length).fill(0);
+    //check if there are any instructions that are dependent on the instruction that is ready to be written
+    if (toBeWrtten.length > 1) {
+
+
+        toBeWrtten.forEach((instruction, index) => {
+            newState.fpAddReservationStations.forEach((station) => {
+                if (station.qj === instruction.tag || station.qk === instruction.tag) {
+                    counts[index]++;
+                }
+                if (station.qk === instruction.tag || station.qj === instruction.tag) {
+                    counts[index]++;
+                }
+            });
+
+            newState.fpMulReservationStations.forEach((station) => {
+                if (station.qj === instruction.tag || station.qk === instruction.tag) {
+                    counts[index]++;
+                }
+                if (station.qk === instruction.tag || station.qj === instruction.tag) {
+                    counts[index]++;
+                }
+            });
+
+            newState.intAddReservationStations.forEach((station) => {
+                if (station.qj === instruction.tag || station.qk === instruction.tag) {
+                    counts[index]++;
+                }
+                if (station.qk === instruction.tag || station.qj === instruction.tag) {
+                    counts[index]++;
+                }
+            });
+
+            newState.intMulReservationStations.forEach((station) => {
+                if (station.qj === instruction.tag || station.qk === instruction.tag) {
+                    counts[index]++;
+                }
+                if (station.qk === instruction.tag || station.qj === instruction.tag) {
+                    counts[index]++;
+                }
+            });
+
+        });
     }
 
-//=================================================================================================================//
-//												MUL/DIV                                                            //
-//=================================================================================================================//
+    //check if counts are all the same
+    let allEqual = counts.every((count) => count === counts[0]);
+    //if they all have the same dependancies choose by first come first serve
+    if (allEqual) {
+        toBeWrtten = toBeWrtten.sort((a, b) => (a.instructionTableIndex ?? 0) - (b.instructionTableIndex ?? 0));
+    }
 
-    if (!busValue && !busTag) {
-        for (const station of fpMulReservationStations) {
-            if (station.busy && station.timeRemaining === 0) {
-                //MISSINNNNNGGGG
-                // busValue = //result mn el execute
-                busTag = station.tag; 
-                station.busy = false; 
-                break;
+
+    const toBeWrttenStation = toBeWrtten[0];
+
+    if (toBeWrttenStation.result !== undefined) {
+        newState.CDB = { tag: toBeWrttenStation.tag, value: toBeWrttenStation.result };
+    }
+
+    //update register file
+
+    if (newState.CDB.tag.startsWith("F")) {
+        newState.fpRegisterFile.forEach((register, index) => {
+            if (register.Q === newState.CDB.tag) {
+                register.value = newState.CDB.value;
+                register.Q = "";
             }
-        }
+        })
     }
 
-//=================================================================================================================//
-//												LOAD                                                               //
-//=================================================================================================================//
-
-if (!busValue && !busTag) {
-        for (const buffer of loadBuffer) {
-            if (buffer.busy && buffer.timeRemaining === 0) {
-                busValue = buffer.address; 
-                busTag = buffer.tag; 
-                buffer.busy = false; 
-                break;
+    if (newState.CDB.tag.startsWith("R")) {
+        newState.intRegisterFile.forEach((register, index) => {
+            if (register.Q === newState.CDB.tag) {
+                register.value = newState.CDB.value;
+                register.Q = "";
             }
-        }
+        })
     }
-
-//=================================================================================================================//
-//									 CHECKING IF THERE IS NOTHING TO WRITE                                         //
-//=================================================================================================================//
-
-    if (!busValue || !busTag) {
-        return systemState;
-    }
-
-//=================================================================================================================//
-//										 BROADCASTING AND UPDATING                                                 //
-//=================================================================================================================//
-    broadcastResult(busValue, busTag, fpAddReservationStations);
-    broadcastResult(busValue, busTag, fpMulReservationStations);
-
-    updateRegisterFiles(busValue, busTag, fpRegisterFile);
-    updateRegisterFiles(busValue, busTag, intRegisterFile);
-
-    for (const entry of instructionTable) {
-        if (entry.instruction.rd && entry.instruction.rd === busTag && entry.writeResult === -1) {
-            entry.execution_complete = `${entry.issue}...${clockCycle}`;
-            entry.writeResult = clockCycle; 
-        }
-    }
-
-    return { ...systemState, clockCycle: clockCycle + 1 }; 
 }
