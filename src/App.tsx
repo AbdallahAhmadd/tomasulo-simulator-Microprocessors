908475191c09dbfde5c6efe3ff29183501a433b2
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-12-12 03:54:19 +0200

    style: format code for improved readability and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import FileUploader from "./components/InstructionParser.tsx";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,7 +11,7 @@ import FpReservationStationInput from "./components/UserInput/FpReservationStati
 import IntReservationStationInput from "./components/UserInput/IntReservationStations.tsx";
 import BufferConfiguration from "./components/UserInput/BufferConfiguration.tsx";
 import RegisterFileConfiguration from "./components/UserInput/RegisterFileConfiguration.tsx";
-import {initializeSystem} from "./simulator.ts";  
+import { initializeSystem } from "./simulator.ts";
 function App() {
   const [instructionQueue, setInstructionQueue] = useState<string[]>([]);
   const [fpAddReservationStationsNums, setfpAddReservationStationsNums] = useState<number>(0);
@@ -26,11 +26,9 @@ function App() {
   const [blockSize, setBlockSize] = useState<number>(0);
   const [latencies, setLatencies] = useState<latencies>();
 
-
   const handleLatancySave = (updatedLatencies: latencies) => {
     console.log("Received latencies:", updatedLatencies);
     setLatencies(updatedLatencies);
-    
   };
   const handleCacheSave = (size: number, block: number) => {
     setCacheSize(size);
@@ -41,7 +39,7 @@ function App() {
     setfpAddReservationStationsNums(fpAdd);
     setfpMulReservationStationsNums(fpMul);
     console.log(`Reservation Station: ${fpAdd}, FpMul: ${fpMul}`);
-  }
+  };
   const handleIntReservationStation = (intAdd: number, intMul: number) => {
     setIntAddReservationStationsNums(intAdd);
     setIntMulReservationStationsNums(intMul);
@@ -51,13 +49,13 @@ function App() {
     setLoadBufferSize(load);
     setStoreBufferSize(store);
     console.log(`Load Buffer Size: ${load}, Store Buffer Size: ${store}`);
-  }
+  };
 
   const handleRegisterSaveConfiguration = (fpReg: number, intReg: number) => {
     setIntRegisterFileSize(intReg);
     setfpRegisterFileSize(fpReg);
     console.log(`Load Register File Size: ${intReg}, Store Register File Size: ${fpReg}`);
-  }
+  };
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (!file) return;
@@ -69,8 +67,8 @@ function App() {
       // Process the file contents
       const lines = fileContents.split(/\r?\n/); // Split into lines
       const cleanedInstructions = lines
-          .map((line) => line.replace(/,/g, '')) // Remove commas
-          .filter((line) => line.length > 0); // Filter out empty lines
+        .map((line) => line.replace(/,/g, "")) // Remove commas
+        .filter((line) => line.length > 0); // Filter out empty lines
 
       setInstructionQueue(cleanedInstructions);
     };
@@ -82,7 +80,7 @@ function App() {
       toast.error("Please provide latency values.");
       return;
     }
-    const SystemConfig : SystemConfig = {
+    const SystemConfig: SystemConfig = {
       fpAddReservationStationsSize: fpAddReservationStationsNums,
       fpMulReservationStationsSize: fpMulReservationStationsNums,
       intAddReservationStationsSize: intAddReservationStationsNums,
@@ -93,32 +91,32 @@ function App() {
       storeBufferSize,
       cacheSize,
       blockSize,
-      latencies
+      latencies,
     };
-    initializeSystem(instructionQueue, SystemConfig);
+    console.log("System State:",initializeSystem(instructionQueue, SystemConfig));
     console.log("SystemConfig:", SystemConfig);
-    console.log('instructionQueue:', instructionQueue);
+    console.log("instructionQueue:", instructionQueue);
   };
 
   return (
-      <>
-        <div>
+    <>
+      <div>
         <FileUploader onChange={handleFileUpload} />
         <LatencyInput onSave={handleLatancySave} />
         <CacheInput onSave={handleCacheSave} />
         <FpReservationStationInput onSave={handleFpReservationStation} />
         <IntReservationStationInput onSave={handleIntReservationStation} />
-        <BufferConfiguration onSave={handleBufferSaveConfiguration}/>;
-        <RegisterFileConfiguration onSave={handleRegisterSaveConfiguration}/>
+        <BufferConfiguration onSave={handleBufferSaveConfiguration} />;
+        <RegisterFileConfiguration onSave={handleRegisterSaveConfiguration} />
         <div className="buttondiv">
-        <button className="execution" onClick={handleExecution}>Start Execution</button>
+          <button className="execution" onClick={handleExecution}>
+            Start Execution
+          </button>
         </div>
-
-        </div>
-        <ToastContainer />
-
+      </div>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
